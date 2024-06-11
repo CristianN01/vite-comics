ab3453262b0064d07d7e0d70bc428981da684fe1
CristianN01 <cristiannechifor71@gmail.com>
2024-06-11 17:35:00 +0200

    aggiunto fine main con aggiunta di shop del merchaindaising ecc

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,7 +4,33 @@ import CurrentSeriesList from './CurrentSeriesList.vue';
 export default {
   data() {
     return {
-      
+      linksDc: [
+        {
+            id: 1,
+            text: 'digital comics',
+            img: "./src/assets/img/buy-comics-digital-comics.png",
+        },
+        {
+            id: 2,
+            text: 'dc merchandise',
+            img: "./src/assets/img/buy-comics-merchandise.png",
+        },
+        {
+            id: 3,
+            text: 'subscription',
+            img: "./src/assets/img/buy-comics-subscriptions.png",
+        },
+        {
+            id: 4,
+            text: 'comic shop locator',
+            img: "./src/assets/img/buy-comics-shop-locator.png",
+        },
+        {
+            id: 5,
+            text: 'dc power visa',
+            img: "./src/assets/img/buy-dc-power-visa.svg",
+        },
+      ]
     }
   },
   components: {
@@ -16,8 +42,18 @@ export default {
 <template>
     <div class="jumbotron">
         </div>
-        <CurrentSeriesList />  
+        <CurrentSeriesList />
+        <section class="buy">
+            <div class="width-flex">
+                <article v-for="article in linksDc" :key="article.id">
+                    <img :src="article.img" :alt="article.id">
+                    <a href="">{{ article.text.toUpperCase() }}</a>
+                </article>
+            </div>
+        </section>  
 </template>
+
+
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
@@ -28,13 +64,36 @@ export default {
         background-image: url(../assets/img/jumbotron.jpg);
         background-size: 100%;
         padding: 12.5rem;
-
-        // img {
-        //     width: 100%;
-        //     background-repeat: no-repeat;
-        //     height: 200px;
-        //     object-fit: cover;
-        // }
     }
+
+    section.buy {
+    background-color: #0282f9;
+    padding: 3.2rem;
+
+    .width-flex {
+        display: flex;
+        max-width: 1300px;
+        margin: 0 auto;
+    }
+
+    article {
+        width: calc(100% / 5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+            width: 55px;
+            height: 55px;
+            object-fit: contain;
+        }
+    }
+
+    a {
+        color: white;
+        text-align: center;
+        margin-left: 1rem;
+    }
+}
 
 </style>
